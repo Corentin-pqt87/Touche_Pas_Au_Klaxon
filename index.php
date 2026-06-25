980b4251679ce28a3d0618546442b771b1432b60
Corentin-pqt87 <propcorentin87@gmail.com>
2026-06-25 11:00:14 +0200

    index.php copy

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/** 
+* on charge le model
+* require "./model/postModel.php";
+*/
+require __DIR__ . "/model/postModel.php";
+/** 
+ * on appelle la fonction getPosts et on stocke les résultats dans une variable $posts
+*/ 
+$posts = getPosts();
+/** 
+ * on charge le temple de la page
+ * require "./templates/home.php";
+*/ 
+require __DIR__ . "/templates/home.php";
