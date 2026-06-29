28966bee327610334ba043f7b0676eb3278eec87
Corentin-pqt87 <propcorentin87@gmail.com>
2026-06-29 17:55:51 +0200

    getPosts

--- Model/postModel.php
+++ Model/postModel.php
@@ -3,5 +3,15 @@
 require __DIR__ . "/../Core/defaultModel.php";
 
 function getPosts() {
-    return findAll('SELECT * FROM posts');
+    return findAll('
+        SELECT 
+            p.*, 
+            u.name AS driver_name,
+            dep.name AS departure_name, dep.location AS departure_location,
+            arr.name AS arrival_name, arr.location AS arrival_location
+        FROM posts p
+        INNER JOIN users u ON p.idusers = u.idusers
+        INNER JOIN agences dep ON p.departure = dep.idagences
+        INNER JOIN agences arr ON p.arrival = arr.idagences
+    ');
 }
